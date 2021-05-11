cc18c1b146d148c3c6a5424930f5664798db7243
manami.takeuchi2 <peettobira@gmail.com>
2021-05-11 14:25:12 +0900

    Initial Commit

---
+++ personRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personRecord
+//
+//  Created by 竹内愛実 on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
